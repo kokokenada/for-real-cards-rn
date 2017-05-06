a98176aae593fc02114cf8f9f72cf075d6b26840
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-05-05 20:48:10 -0400

    register page (#1)
    * Added register button on the home screen and added a username field in the register page
    
    * Added a password field, a sign in button that only sends them to the login page for now and a button to send them back to the home page

--- src/features/start/register.tsx
+++ src/features/start/register.tsx
@@ -1,15 +1,115 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity}
+from 'react-native';
+
 
 interface Props { }
-interface State { }
+interface State {
+
+}
+
+const personIcon = require("../../../src/features/start/login1_person.png");
 
 export default class Register extends Component<Props, State> {
+  state = {
+    id: '',
+    password: '',
+  }
+
+  idChange(id) {
+    this.setState({ id })
+  }
+
+  passwordChange(password) {
+    this.setState({ password })
+  }
+
   render() {
+    const { navigate } = this.props.navigation
     return (
-      <View style={{flex: 1}}>
-        <Text>Register</Text>
+      <View style={styles.container}>
+          <View style={styles.wrapper}>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={personIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput
+                name='id'
+                placeholder='Username'
+                placeholderTextColor="#FFF"
+                style={styles.input}
+                value={this.state.id}
+                onChangeText={text => this.idChange(text)}
+              />
+            </View>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={personIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput
+                placeholder='Password'
+                placeholderTextColor="#FFF"
+                style={styles.input}
+                value={this.state.password}
+                onChangeText={text => this.passwordChange(text)}
+                secureTextEntry
+              />
+            </View>
+            <TouchableOpacity activeOpacity={.5}>
+              <View style={styles.button}>
+                <Text onPress={() => navigate('Login')} style={styles.buttonText}>Sign In</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={.5}>
+              <View style={styles.button}>
+                <Text onPress={() => navigate('Home')} style={styles.buttonText}>Home</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
       </View>
     )
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    paddingVertical: 30,
+  },
+  iconWrap: {
+    paddingHorizontal: 7,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    height: 20,
+    width: 20,
+  },
+  inputWrap: {
+    flexDirection: "row",
+    marginVertical: 5,
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: "#CCC"
+  },
+  input: {
+    flex: 1,
+    paddingBottom: 20,
+    marginTop: 15,
+    height: 40,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 18,
+  },
+})
