bf9ca7293d7ff497045f333d1c6a6f22b05405ec
Ken Ono <ken.ono.gm@gmail.com>
2017-05-06 07:47:09 -0400

    removed commented out imports

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import { AppRegistry } from 'react-native';
 
 import { App } from './app';
-//import Login from './features/start/login';
-//import AppRouter from './features/router/router';
 
 AppRegistry.registerComponent('ForRealCards', () => App);
