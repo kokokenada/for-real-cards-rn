92c9c72c9e81f3b95c2aaea51299393c87edb38d
Ken Ono <ken.ono.gm@gmail.com>
2017-05-06 11:40:47 -0400

    made detecting URL from meteor robust from errors, picked up URL from configuration if can't get it from meteor, added debugging

--- src/common-app/redux-packages/connect/connect-service.ts
+++ src/common-app/redux-packages/connect/connect-service.ts
@@ -1,5 +1,7 @@
 import 'meteor-client';
 declare let Package:any; declare let Accounts:any; // Couldn't find typings
+declare let __meteor_runtime_config__;
+
 import { _ } from 'underscore';
 
 // Make an abstract parent and children that implement specific backend
@@ -11,10 +13,26 @@ export class ConnectService {
   }
 
   static getServerURL():string {
-    return Meteor.absoluteUrl();
+    let result = '';
+    let configured = '';
+    if (typeof __meteor_runtime_config__ === 'undefined') {
+      console.warn(' __meteor_runtime_config__ is undefined')
+    } else {
+      if ( typeof __meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL !== 'undefined')
+        configured =  __meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL;
+    }
+    try {
+      result = Meteor.absoluteUrl();
+    } catch (e) {
+      console.warn(e);
+      result = configured;
+      console.warn('Using ' + configured);
+    }
+    return result;
   }
 
   static setServerTo(app_url) {
+    console.log('setting url' + app_url)
     Meteor.connection = Meteor.connect(app_url);
     _.each(['subscribe', 'methods', 'call', 'apply', 'status','reconnect','disconnect'], function (name) {
       Meteor[name] = _.bind(Meteor.connection[name], Meteor.connection);
@@ -24,6 +42,7 @@ export class ConnectService {
   }
 
   static reconnect() {
+    console.log('reconnecting')
     Meteor.reconnect();
   }
 
