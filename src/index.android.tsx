a10cd4f43385b6e485169ea7aa5ccc917bdaea17
Ken Ono <ken.ono.gm@gmail.com>
2017-05-06 07:46:38 -0400

    fixed android

--- src/index.android.tsx
+++ src/index.android.tsx
@@ -1,55 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-interface Props { }
-interface State { }
+import { AppRegistry } from 'react-native';
 
-export default class ForRealCards extends Component<Props, State> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! Wow?
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import { App } from './app';
 
-const styles: any = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ForRealCards', () => ForRealCards);
+AppRegistry.registerComponent('ForRealCards', () => App);
