45b29de2284b2a7146430ebabf751d85f5e6be43
Ken Ono <ken.ono.gm@gmail.com>
2017-05-10 07:05:12 -0400

    template for dev environment

---
+++ src/env-template.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig  = {
+  apiKey: "your dev environment",
+  authDomain: "your dev environment",
+  databaseURL: "your dev environment",
+  projectId: "your dev environment",
+  storageBucket: "your dev environment",
+  messagingSenderId: "your dev environment"
+};
