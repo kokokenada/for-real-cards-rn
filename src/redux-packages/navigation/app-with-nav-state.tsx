2a790c7009cfb919a4b17b8def6ee9efcdbb078d
Ken Ono <ken.ono.gm@gmail.com>
2017-05-18 08:09:28 -0400

    fix build, move to TS

--- src/redux-packages/navigation/app-with-nav-state.tsx
+++ src/redux-packages/navigation/app-with-nav-state.tsx
@@ -1,7 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {TabNavigator, addNavigationHelpers, StackNavigator} from 'react-navigation'
 import {AppNavigator} from './router';
-import {NavigationActions} from './navigation-actions';
 import {NAVIGATE_PACKAGE_NAME} from './navigation-package';
 import {connect} from 'react-redux';
 
@@ -11,35 +10,14 @@ interface Props {
 }
 interface State {
 }
-/*  Hmm.  My TypeScript JavaScript knowledhe has been exceeded.  Why isn't this the same as the JavaScript below?
 export class AppWithNavigationState extends Component<Props, State> {
-  dispatch;
-  nav;
-
-  constructor({dispatch, nav}) {
-    super();
-    console.log('constructor')
-    console.log(arguments)
-    this.dispatch = dispatch;
-    this.nav = nav;
-
-  }
 
   render() {
     return (
-      <AppNavigator navigation={addNavigationHelpers({dispatch: this.dispatch, state: this.nav})}/>
+      <AppNavigator navigation={addNavigationHelpers({dispatch: this.props.dispatch, state: this.props.nav})}/>
     );
   }
 }
-*/
-
-const AppWithNavigationState: any = ({dispatch, nav}) => (
-  <AppNavigator navigation={addNavigationHelpers({dispatch, state: nav})}/>
-);
-AppWithNavigationState.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  nav: PropTypes.object.isRequired,
-};
 
 const mapStateToProps = state => {
   return ({
